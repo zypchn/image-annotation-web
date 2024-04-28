0edafb801a2e96d4ac6866d80e312f50dd5bdd9f
Baran Bingöl <baranbingol212121@gmail.com>
2024-04-28 15:59:38 +0300

    revert back old code

--- frontend/src/containers/Canvas.jsx
+++ frontend/src/containers/Canvas.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useMemo, useRef, useState, useEffect } from "react";
 import PolygonAnnotation from "../components/PolygonAnnotation.jsx";
 import { Stage, Layer, Image } from "react-konva";
 import Button from "../components/Button.jsx";
-
 const videoSource = "src/containers/tablet4.jpeg";
 
 const wrapperStyle = {
@@ -11,117 +10,165 @@ const wrapperStyle = {
     marginTop: 20,
     backgroundColor: "aliceblue",
 };
-
 const columnStyle = {
     display: "flex",
+    justifyContent: "center",
     flexDirection: "column",
     alignItems: "center",
     marginTop: 20,
     backgroundColor: "aliceblue",
 };
-
 const Canvas = () => {
     const [image, setImage] = useState();
     const imageRef = useRef(null);
-    const [polygons, setPolygons] = useState([]); // birden fazla polygon depolamak için state
+    const dataRef = useRef(null);
+    const [points, setPoints] = useState([]);
     const [size, setSize] = useState({});
-
+    const [flattenedPoints, setFlattenedPoints] = useState();
+    const [position, setPosition] = useState([0, 0]);
+    const [isMouseOverPoint, setMouseOverPoint] = useState(false);
+    const [isPolyComplete, setPolyComplete] = useState(false);
+    const videoElement = useMemo(() => {
+        const element = new window.Image();
+        element.width = 650;
+        element.height = 500;
+        element.src = videoSource;
+        return element;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [videoSource]); //it may come from redux, so it may be dependency that's why I left it as dependecny...
     useEffect(() => {
-        const imageObj = new window.Image();
-        imageObj.src = videoSource;
-        imageObj.onload = () => {
+        const onload = function () {
             setSize({
-                width: imageObj.width,
-                height: imageObj.height,
+                width: videoElement.width,
+                height: videoElement.height,
             });
-            setImage(imageObj);
-            imageRef.current.cache();
-            imageRef.current.getLayer().batchDraw();
+            setImage(videoElement);
+            imageRef.current = videoElement;
         };
-    }, []);
-
+        videoElement.addEventListener("load", onload);
+        return () => {
+            videoElement.removeEventListener("load", onload);
+        };
+    }, [videoElement]);
+    const getMousePos = (stage) => {
+        return [stage.getPointerPosition().x, stage.getPointerPosition().y];
+    };
+    //drawing begins when mousedown event fires.
     const handleMouseDown = (e) => {
+        if (isPolyComplete) return;
         const stage = e.target.getStage();
-        const mousePos = stage.getPointerPosition();
-        const newPolygons = [...polygons];
-
-        if (newPolygons.length === 0 || newPolygons[newPolygons.length - 1].isComplete) {
-            newPolygons.push({
-                points: [mousePos.x, mousePos.y],
-                isComplete: false,
-            });
-            setPolygons(newPolygons);
+        const mousePos = getMousePos(stage);
+        if (isMouseOverPoint && points.length >= 3) {
+            setPolyComplete(true);
         } else {
-            const lastPolygon = newPolygons[newPolygons.length - 1];
-            lastPolygon.points = [...lastPolygon.points, mousePos.x, mousePos.y];
-            setPolygons([...newPolygons]);
+            setPoints([...points, mousePos]);
         }
     };
-
-    const handleFinishPolygon = () => {
-        const lastPolygon = polygons[polygons.length - 1];
-        lastPolygon.isComplete = true;
-        setPolygons([...polygons]);
+    const handleMouseMove = (e) => {
+        const stage = e.target.getStage();
+        const mousePos = getMousePos(stage);
+        setPosition(mousePos);
     };
-
-    const handleUndo = () => {
-        const newPolygons = [...polygons];
-        if (newPolygons.length > 0 && !newPolygons[newPolygons.length - 1].isComplete) {
-            const lastPolygon = newPolygons[newPolygons.length - 1];
-            lastPolygon.points.pop();
-            if (lastPolygon.points.length === 0) {
-                newPolygons.pop();
-            }
-            setPolygons(newPolygons);
+    const handleMouseOverStartPoint = (e) => {
+        if (isPolyComplete || points.length < 3) return;
+        e.target.scale({ x: 3, y: 3 });
+        setMouseOverPoint(true);
+    };
+    const handleMouseOutStartPoint = (e) => {
+        e.target.scale({ x: 1, y: 1 });
+        setMouseOverPoint(false);
+    };
+    const handlePointDragMove = (e) => {
+        const stage = e.target.getStage();
+        const index = e.target.index - 1;
+        const pos = [e.target._lastPos.x, e.target._lastPos.y];
+        if (pos[0] < 0) pos[0] = 0;
+        if (pos[1] < 0) pos[1] = 0;
+        if (pos[0] > stage.width()) pos[0] = stage.width();
+        if (pos[1] > stage.height()) pos[1] = stage.height();
+        setPoints([...points.slice(0, index), pos, ...points.slice(index + 1)]);
+    };
+    
+    useEffect(() => {
+        setFlattenedPoints(
+            points
+            .concat(isPolyComplete ? [] : position)
+            .reduce((a, b) => a.concat(b), [])
+        );
+    }, [points, isPolyComplete, position]);
+    const undo = () => {
+        setPoints(points.slice(0, -1));
+        setPolyComplete(false);
+        setPosition(points[points.length - 1]);
+    };
+    const reset = () => {
+        setPoints([]);
+        setPolyComplete(false);
+    };
+    const handleGroupDragEnd = (e) => {
+        //drag end listens other children circles' drag end event
+        //...that's, why 'name' attr is added, see in polygon annotation part
+        if (e.target.name() === "polygon") {
+            let result = [];
+            let copyPoints = [...points];
+            copyPoints.map((point) =>
+                result.push([point[0] + e.target.x(), point[1] + e.target.y()])
+            );
+            e.target.position({ x: 0, y: 0 }); //needs for mouse position otherwise when click undo you will see that mouse click position is not normal:)
+            setPoints(result);
         }
     };
-
-    const handleReset = () => {
-        setPolygons([]);
-    };
-
-    const handleRemoveLastPolygon = () => {
-        if (polygons.length > 0) {
-            const newPolygons = [...polygons];
-            newPolygons.pop(); // Remove the last polygon
-            setPolygons(newPolygons);
-        }
-    };
-
+    
     return (
         <div style={wrapperStyle}>
             <div style={columnStyle}>
                 <Stage
                     width={size.width || 650}
-                    height={size.height || 500}
+                    height={size.height || 302}
+                    onMouseMove={handleMouseMove}
                     onMouseDown={handleMouseDown}
                 >
                     <Layer>
                         <Image
                             ref={imageRef}
                             image={image}
+                            x={0}
+                            y={0}
                             width={size.width}
                             height={size.height}
                         />
-                        {polygons.map((polygon, index) => (
-                            <PolygonAnnotation
-                                key={index}
-                                points={polygon.points}
-                                isComplete={polygon.isComplete}
-                            />
-                        ))}
+                        <PolygonAnnotation
+                            points={points}
+                            flattenedPoints={flattenedPoints}
+                            handlePointDragMove={handlePointDragMove}
+                            handleGroupDragEnd={handleGroupDragEnd}
+                            handleMouseOverStartPoint={handleMouseOverStartPoint}
+                            handleMouseOutStartPoint={handleMouseOutStartPoint}
+                            isFinished={isPolyComplete}
+                        />
                     </Layer>
                 </Stage>
-                <div style={{ marginTop: 10 }}>
-                    <Button name="Polygonu Bitir" onClick={handleFinishPolygon} />
-                    <Button name="Geri Al" onClick={handleUndo} />
-                    <Button name="Hepsini Sıfırla" onClick={handleReset} />
-                    <Button name="Bir Önceki Poligonu Sil" onClick={handleRemoveLastPolygon} />
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Button name="Undo" onClick={undo} />
+                    <Button name="Reset" onClick={reset} />
                 </div>
             </div>
-            <div style={{ marginLeft: 20, maxWidth: 300 }}>
-                <h3>Polygon Koordinatları:</h3> 
-                <pre>{JSON.stringify(polygons, null, 2)}</pre>
+            <div
+                ref={dataRef}
+                style={{
+                    width: 375,
+                    height: 302,
+                    boxShadow: ".5px .5px 5px .4em rgba(0,0,0,.1)",
+                    marginTop: 20,
+                }}
+            >
+                <pre style={{ whiteSpace: "pre-wrap" }}>{JSON.stringify(points)}</pre>
             </div>
         </div>
     );
