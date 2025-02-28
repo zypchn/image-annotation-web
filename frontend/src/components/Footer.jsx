d313de2e261bc57cec319d8350a5ae6f331d4abb
Ural Altan Bozkurt <uralaltan10@gmail.com>
2025-03-01 02:14:28 +0300

    Modified footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,21 @@
 const Footer = () => {
-    return (
-        <footer className={"footer"}>
-            <p> &copy; {new Date().getFullYear()} Copyright: Developed by <b>Zeynep Cahan </b> </p>
-        </footer>
-    );
+  return (
+    <footer
+      className={"footer"}
+      style={{
+        width: "100%",
+        borderRadius: "999px",
+        margin: "0 auto",
+        padding: "10px",
+      }}
+    >
+      <p>
+        {" "}
+        &copy; {new Date().getFullYear()} Copyright: Developed by{" "}
+        <b>Zeynep Cahan </b>{" "}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
